cb0cff4533265ccd16603ba800694d7792de65b8
hye1ee <hyewon0809@kaist.ac.kr>
2024-03-12 10:08:42 -0400

    [chore] Fix visibility issue

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ document.getElementById("window-inside").addEventListener("click", (evt) => {
 });
 
 document.getElementById("before-start").addEventListener("mouseover", () => {
+  document.getElementById("before-overlay").style.visibility = "visible";
   document.getElementById("before-overlay").style.opacity = 1;
 });
 document.getElementById("before-start").addEventListener("mouseout", () => {
   document.getElementById("before-overlay").style.opacity = 0;
+  document.getElementById("before-overlay").style.visibility = "hidden";
 });
